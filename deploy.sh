a97a3e4e6ecb0e1a1ce9be67bbe6a536669332a8
haoliangyu <njkon@foxmail.com>
2018-03-12 00:31:01 -0400

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-eval $(aws ecr get-login --region eu-west-1)
-docker build .
-docker tag haoliangyu/ecs-auto-deploy:latest $ECS_REPO_URI
+eval $(aws ecr get-login --region us-east-1)
+docker build -t $ECS_REPO_URI .
 docker push $ECS_REPO_URI
